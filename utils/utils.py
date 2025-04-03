3244d2a5bf4fc96e6e1ce265d90e19fad44dd59c
floleony <72750667+floleony@users.noreply.github.com>
2025-04-03 22:16:20 +0700

    Update utils.py

--- utils/utils.py
+++ utils/utils.py
@@ -1,7 +1,8 @@
+from sklearn.metrics import balanced_accuracy_score, recall_score, specificity_score, roc_auc_score, matthews_corrcoef
 from sklearn.base import BaseEstimator
 import numpy as np
 
-def evaluate(model: BaseEstimator, X: np.ndarray, y: np.ndarray):
+def evaluate(model: BaseEstimator, X: np.ndarray, y: np.ndarray, average='weighted'):
     # try:
     y_true, y_pred, pred_prob = y, model.predict(X), model.predict_proba(X)
     y_prob = net.predict_proba(X)
