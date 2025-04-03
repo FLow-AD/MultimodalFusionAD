b5aefaf988f58c0c34b363f186888c205f9eacd3
floleony <72750667+floleony@users.noreply.github.com>
2025-04-03 22:14:16 +0700

    Create utils.py

---
+++ utils/utils.py
@@ -0,0 +1,17 @@
+from sklearn.base import BaseEstimator
+import numpy as np
+
+def evaluate(model: BaseEstimator, X: np.ndarray, y: np.ndarray):
+    # try:
+    y_true, y_pred, pred_prob = y, model.predict(X), model.predict_proba(X)
+    y_prob = net.predict_proba(X)
+
+    metrics = {
+        'BA': balanced_accuracy_score(y_true, y_pred)
+        'SEN': recall_score(y_true, y_pred, average=average)
+        'SPE': specificity_score(y_true, y_pred, average=average)
+        'AUC': roc_auc_score(y_true, pred_prob, multi_class="ovr", average=average)
+        'MCC': matthews_corrcoef(y_true, y_pred)
+    }
+
+    return metrics
