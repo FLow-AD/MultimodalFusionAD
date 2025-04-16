6d92a76c47f7ae9023c386873cf94c8f8080902c
FL <72750667+FLow-AD@users.noreply.github.com>
2025-04-16 17:04:23 +0800

    Update utils.py

--- utils/utils.py
+++ utils/utils.py
@@ -8,10 +8,21 @@ def evaluate(model: BaseEstimator, X: np.ndarray, y: np.ndarray, average='weight
     y_prob = net.predict_proba(X)
 
     metrics = {
-        'BA': balanced_accuracy_score(y_true, y_pred)
-        'SEN': recall_score(y_true, y_pred, average=average)
-        'SPE': specificity_score(y_true, y_pred, average=average)
-        'AUC': roc_auc_score(y_true, pred_prob, multi_class="ovr", average=average)
+        'BA': balanced_accuracy_score(y_true, y_pred),
+        'SEN': recall_score(y_true, y_pred, average=average),
+        'SPE': specificity_score(y_true, y_pred, average=average),
+        'AUC': roc_auc_score(y_true, pred_prob, multi_class="ovr", average=average),
+        'MCC': matthews_corrcoef(y_true, y_pred)
+    }
+
+    return metrics
+
+def evaluate_voting(y_true, y_pred, pred_prob, average='weighted'):
+    metrics = {
+        'BA': balanced_accuracy_score(y_true, y_pred),
+        'SEN': recall_score(y_true, y_pred, average=average),
+        'SPE': specificity_score(y_true, y_pred, average=average),
+        'AUC': roc_auc_score(y_true, pred_prob, multi_class="ovr", average=average),
         'MCC': matthews_corrcoef(y_true, y_pred)
     }
 
