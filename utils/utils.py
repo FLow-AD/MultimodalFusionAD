e088905780fe8247c2b8b64a9c0a675843131f85
FL <72750667+FLow-AD@users.noreply.github.com>
2025-04-16 17:05:13 +0800

    Update utils.py

--- utils/utils.py
+++ utils/utils.py
@@ -16,3 +16,14 @@ def evaluate(model: BaseEstimator, X: np.ndarray, y: np.ndarray, average='weight
     }
 
     return metrics
+
+def evaluate_voting(y_true, y_pred, pred_prob, average='weighted'):
+    metrics = {
+        'BA': balanced_accuracy_score(y_true, y_pred),
+        'SEN': recall_score(y_true, y_pred, average=average),
+        'SPE': specificity_score(y_true, y_pred, average=average),
+        'AUC': roc_auc_score(y_true, pred_prob, multi_class="ovr", average=average),
+        'MCC': matthews_corrcoef(y_true, y_pred)
+    }
+
+    return metrics
